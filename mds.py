- correct the formula of euc dist - use only new models in the ensemble machine loss - do scikit-learn MDS in the fitting phase instead of gradient descent   MDS - add the update phase - add a plotting module to see the embedding - replace the argmax(probas,axis=1) by sum(probas*arange(nb_classes),   axis=1) as a representation of models

mds.py
@@ -6,7 +6,8 @@ def MDS_loss(dist_X, dist_Y):
 
 
 def euclidian_dist(X):
-    return T.dot(X, X.T)
+    s = (X.dimshuffle(0, 1, 'x') - X.dimshuffle('x', 1, 0)) ** 2
+    return (s.sum(axis=1))
 
 
 def hamming_dist(X):

model.py
@@ -1,22 +1,26 @@
 import mds
 import theano
 import theano.tensor as T
-from lasagne import easy, init, updates
+from lasagne import easy, updates, layers, init, nonlinearities, objectives
 import numpy as np
+from sklearn.manifold import MDS
 
-def ensemble_machine_model_loss(y_new, z_new, y_old, z_old, dist_y_func, dist_z_func):
+
+from lasagne.easy import LightweightModel
+def ensemble_machine_model_loss(y_new, z_new,
+                                y_old, z_old,
+                                dist_y_func, dist_z_func):
     # y_new : (nb new models, nb outputs) predicted points by the new models
     # z_new : (nb new models, embedding dim) wanted points in the
     #         embedding
     # y_old : (nb models, nb outputs)
     # z_old : (nb models, embedding dim)
 
+    y = T.concatenate([y_old, y_new], axis=0)
+    dist_y = dist_y_func(y)[y_old.shape[0]:]
 
-    y = T.concatenate([y_new, y_old], axis=0)
-    dist_y = dist_y_func(y)
-
-    z = T.concatenate([z_new, z_old], axis=0)
-    dist_z = dist_z_func(z)
+    z = T.concatenate([z_old, z_new], axis=0)
+    dist_z = dist_z_func(z)[z_old.shape[0]:]
 
     return mds.MDS_loss(dist_y, dist_z)
 
@@ -24,8 +28,9 @@ def ensemble_machine_model_loss(y_new, z_new, y_old, z_old, dist_y_func, dist_z_
 class EnsembleMachine(object):
 
     def __init__(self, n_components=2,
-                       dist_y=mds.euclidian_dist, dist_z=mds.euclidian_dist,
-                       batch_optimizer=None):
+                 dist_y=mds.euclidian_dist,
+                 dist_z=mds.euclidian_dist,
+                 batch_optimizer=None):
 
         self.n_components = n_components
         if batch_optimizer is None:
@@ -37,12 +42,20 @@ class EnsembleMachine(object):
         self.Z = None
 
     def fit(self, Y):
+        mds = MDS(n_components=2, dissimilarity='precomputed')
+
+        #dis = (Y[:, :, np.newaxis] != Y.T[np.newaxis, :, :]).sum(axis=1) # hamming
+        dis =  np.sqrt(((Y[:, :, np.newaxis] - Y.T[np.newaxis, :, :])**2).sum(axis=1))
+        Z = mds.fit_transform(dis)
+        self.Y = theano.shared(Y, borrow=True)
+        self.Z = theano.shared(Z, borrow=True)
+        return self
+
         nb_examples = Y.shape[0]
 
         Z_init = np.random.uniform(size=(Y.shape[0], self.n_components))
         Z_init = Z_init.astype(theano.config.floatX)
         self.Z = theano.shared(Z_init, borrow=True)
-
 
         Y = theano.shared(Y, borrow=True)
         Y_batch = T.matrix('Y_batch')
@@ -68,18 +81,24 @@ class EnsembleMachine(object):
             updates=updates,
             givens=givens
         )
-        nb_batches = easy.get_nb_batches(nb_examples, self.batch_optimizer.batch_size)
+        nb_batches = easy.get_nb_batches(nb_examples,
+                                         self.batch_optimizer.batch_size)
         self.batch_optimizer.optimize(nb_batches, iter_train)
-        self.stress_ = theano.function([], loss, givens={Y_batch : Y, batch_index: 0})()
-        self.Y = theano.shared(theano.function([], d_y, givens={Y_batch: Y})(), borrow=True)
+
+        get_stress = theano.function([], loss,
+                                     givens={Y_batch: Y, batch_index: 0})
+        self.stress_ = get_stress()
+        self.Y = Y
         return self
 
-    def update(self, X, models_new, Z_new, batch_optimizer=None):
-
+    def update(self, X, y, models_new, Z_new, batch_optimizer=None, lambda_=1.):
+        nb_examples = X.shape[0]
         if batch_optimizer is None:
-            batch_optimizer = easy.BatchOptimizer()
+            batch_optimizer = easy.BatchOptimizer(batch_size=X.shape[0],
+                                                  verbose=1)
 
-        X_batch = T.matrix()
+        X_batch = T.matrix('X_batch')
+        y_batch = T.ivector('y_batch')
         batch_index = T.iscalar('batch_index')
         batch_slice = easy.get_batch_slice(batch_index,
                                            batch_optimizer.batch_size)
@@ -88,82 +107,159 @@ class EnsembleMachine(object):
         # Z_new (nb_new_models, nb_components)
         # models_new : list of nb_new_models LightweightModel
         outputs = []
+        probas = []
         for model in models_new:
-            output = model.get_output(X_batch).flatten().dimshuffle('x', 0)
-            outputs.append(output) # (1, nb examples * nb_outputs)
+            output, = model.get_output(X_batch)
+            probas.append(output)
+            #output = T.argmax(output, axis=1)
+            output = (output * T.arange(model.output_layers[0].num_units)).sum(axis=1)
+            #output = T.cast(output, theano.config.floatX)
+            output = output.dimshuffle('x', 0)
+            outputs.append(output)  # (1, nb examples)
 
-        Y_new =  T.concatenate(outputs, axis=0) # (nb_new_models, nb_examples * nb_outputs)
+        # (nb_new_models, nb_examples)
+        Y_new = T.concatenate(outputs, axis=0)
+        loss_ensemble_machine = ensemble_machine_model_loss(Y_new, Z_new,
+                                                            self.Y, self.Z,
+                                                            self.dist_y, self.dist_z)
+        loss_accuracy = 0.
+        for pr in probas:
+            loss_accuracy += -T.mean(T.log(pr)[T.arange(y_batch.shape[0]), y_batch])
 
-        loss = ensemble_machine_model_loss(Y_new, Z_new, self.Y, self.Z, self.dist_y, self.dist_z)
-        all_params = list(set(model.get_all_params() for model in models_new))
+        loss = lambda_ * loss_accuracy + (1 - lambda_) * loss_ensemble_machine
+        all_params = list(set(param
+                              for model in models_new
+                              for param in model.get_all_params()))
 
         opti_function, opti_kwargs = batch_optimizer.optimization_procedure
         updates = opti_function(loss, all_params, **opti_kwargs)
 
         X = theano.shared(X, borrow=True)
+        y = theano.shared(y, borrow=True)
         givens = {
             X_batch: X[batch_slice],
+            y_batch : y[batch_slice]
         }
         iter_train = theano.function(
             [batch_index], loss,
             updates=updates,
             givens=givens
         )
-        nb_batches = easy.get_nb_batches(nb_examples, self.batch_optimizer.batch_size)
-        self.batch_optimizer.optimize(nb_batches, iter_train)
+
+        for i, model in enumerate(models_new):
+            pr = probas[i]
+            predict = theano.function([X_batch], outputs[i][0, :])
+            predict_proba = theano.function([X_batch], pr)
+            self.__dict__["predict_%d" % (i,)] = predict
+            self.__dict__["predict_proba_%d" % (i,)] = predict_proba
+        self.predict = self.predict_0
+        self.predict_proba = self.predict_proba_0
+
+        self.get_loss = theano.function([X_batch, y_batch], loss)
+        self.get_loss_ensemble_machine = theano.function([X_batch], loss_ensemble_machine)
+        self.get_loss_accuracy = theano.function([X_batch, y_batch], loss_accuracy)
+
+        y = T.concatenate([self.Y, Y_new], axis=0)
+        dist_y = self.dist_y(y)
+
+        z = T.concatenate([self.Z, Z_new], axis=0)
+        dist_z = self.dist_z(z)
+
+        self.get_dist_y = theano.function([X_batch], dist_y)
+        self.get_dist_z = theano.function([], dist_z)
+
+        nb_batches = easy.get_nb_batches(nb_examples,
+                                         batch_optimizer.batch_size)
+        batch_optimizer.optimize(nb_batches, iter_train)
         return self
 
-def build_Y(X, models):
+
+def build_Y(X, models, n_classes):
     Y = np.zeros((len(models), X.shape[0]))
     for i, (name, model) in enumerate(models):
-        Y[i, :] = model.predict(X)
+        Y[i, :] = (model.predict_proba(X) * np.arange(n_classes)).sum(axis=1)
+        #Y[i, :] = model.predict(X)
     return Y
 
 if __name__ == "__main__":
-    from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+    from sklearn.ensemble import (RandomForestClassifier, AdaBoostClassifier,
+                                  GradientBoostingClassifier)
+    from sklearn.tree import DecisionTreeClassifier
     from sklearn.linear_model import LogisticRegression
     from sklearn.svm import SVC
     from sklearn.datasets import make_classification
-
-    import matplotlib.pyplot as plt
+    from sklearn.preprocessing import StandardScaler
+#   import matplotlib.pyplot as plt
     from plotting import plot_model_embeddings
-
-    X, y = make_classification(100)
+    np.random.seed(0)
+    y_dim = 4
+    n_classes = y_dim
+    X, y = make_classification(100, n_classes=y_dim, n_informative=10)
     X = X.astype(theano.config.floatX)
-
+    X = StandardScaler().fit_transform(X)
 
     models = [
-        ('rf', RandomForestClassifier()),
-        ('ada', AdaBoostClassifier()),
+        ('rf_1', RandomForestClassifier(n_estimators=10, max_depth=5)),
+        ('rf_2', RandomForestClassifier(n_estimators=50, max_depth=2)),
+        ('rf_3', RandomForestClassifier(n_estimators=100, max_depth=5)),
+        ('ada_1', AdaBoostClassifier(n_estimators=10)),
+        ('ada_2', AdaBoostClassifier(n_estimators=12)),
         ('logreg', LogisticRegression()),
-        ('svm', SVC()),
+        ('svm', SVC(probability=True)),
+        ('gb_1', GradientBoostingClassifier(max_depth=3)),
+        ('gb_2', GradientBoostingClassifier(max_depth=8)),
     ]
 
     for name, model in models:
         model.fit(X, y)
-    Y = build_Y(X, models)
+    Y = build_Y(X, models, n_classes)
     Y = Y.astype(theano.config.floatX)
-
+    opti = (updates.sgd, {"learning_rate": 0.01})
     batch_optimizer = easy.BatchOptimizer(verbose=1,
                                           batch_size=Y.shape[0],
-                                          max_nb_epochs=100,
-                                          optimization_procedure=(updates.Adam, {"learning_rate": 0.3}))
-    es = EnsembleMachine(batch_optimizer=batch_optimizer)
+                                          max_nb_epochs=10,
+                                          optimization_procedure=opti)
+    es = EnsembleMachine(n_components=2,
+                         dist_y=mds.euclidian_dist,
+                         batch_optimizer=batch_optimizer)
     es.fit(Y)
+    #plot_model_embeddings(models, es.Z.get_value())
 
-    plot_model_embeddings(models, es.Z.get_value())
-    """
+
     x_in = layers.InputLayer(shape=(None, X.shape[1]))
-    h = layers.DenseLayer(x_in, num_units=z_dim,
-                          W=init.GlorotUniform(),
+    h = layers.DenseLayer(x_in, num_units=100,
+                          W=init.GlorotNormal(),
                           nonlinearity=nonlinearities.rectify)
-    z_out = layers.DenseLayer(h, num_units=z_dim,
+    z_out = layers.DenseLayer(h, num_units=y_dim,
                               W=init.GlorotUniform(),
-                              nonlinearity=nonlinearities.rectify)
-    nnet_x_to_z = LightweightModel([x_in],
-                                   [z_out])
+                              nonlinearity=nonlinearities.softmax)
+    model = LightweightModel([x_in],
+                             [z_out])
+    models_new = [model]
+    Z_new = [
+        [4, -3]
+    ]
+    Z_new = np.array(Z_new, dtype=theano.config.floatX)
 
-    models = [model]
-
-    es.update()
-    """
+    class MyBatchOptimizer(easy.BatchOptimizer):
+        def iter_update(self, epoch, nb_batches, iter_update_batch):
+                super(MyBatchOptimizer, self).iter_update(epoch, nb_batches, iter_update_batch)
+                if epoch == self.max_nb_epochs - 1:
+                    print(es.get_dist_y(X))
+                    print(es.get_dist_z())
+                print("loss ensemble machine : ", es.get_loss_ensemble_machine(X))
+                print("loss accuracy : ", es.get_loss_accuracy(X, y))
+                print("loss :", es.get_loss(X, y))
+    batch_optimizer = MyBatchOptimizer(verbose=1,
+                                       max_nb_epochs=100,
+                                       optimization_procedure=(updates.adadelta, {"learning_rate": 1.}))
+    es.update(X, y, models_new, Z_new,
+              batch_optimizer=batch_optimizer,
+              lambda_=0.5)
+    models_updated = models + [("new_%d" % (i,), es) for i, m in enumerate(models_new)]
+    Y = build_Y(X, models_updated, n_classes)
+    Y = Y.astype(theano.config.floatX)
+    es = EnsembleMachine(n_components=2,
+                         dist_y=mds.euclidian_dist)
+    es.fit(Y)
+    plot_model_embeddings(models_updated, es.Z.get_value())

plotting.py
@@ -1,65 +1,46 @@
 from __future__ import division
-
-import itertools
+import matplotlib.pyplot as plt
 
 import numpy as np
 
-from bokeh.plotting import ColumnDataSource, figure, output_file, show
-from bokeh.models import HoverTool
+def plot_model_embeddings(models, Z, title="untitled"):
 
+    plt.ion()
 
-def plot_model_embeddings(models, Z, title="untitled", output="out.html"):
-    # Create a set of tools to use
-    TOOLS="pan,wheel_zoom,box_zoom,reset,hover"
     x = Z[:, 0]
     y = Z[:, 1]
-    radii = 1.
-    inds = np.arange(1, Z.shape[0] + 1)
-    colors = inds
+    radii = 300.
     names = [name for name, model in models]
-    # We need to put these data into a ColumnDataSource
-    source = ColumnDataSource(
-        data=dict(
-                x=x,
-                y=y,
-                radius=radii,
-                colors=colors,
-                name=names
-            )
-    )
 
-    output_file(output)
+    models_kinds = [name.split("_")[0] for name in names]
+    kinds = list(set(models_kinds))
+    mapping = {k: i + 1 for i, k in enumerate(kinds)}
+    colors = [mapping[k] for k in models_kinds]
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(x, y, s=radii,
+               c=colors, alpha=0.5, picker=True)
 
-    p = figure(title=title, tools=TOOLS)
+    annotations = []
+    for name, x_i, y_i in zip(names, x, y):
+        a = ax.annotate(name,
+                     xy=(x_i, y_i),
+                     xytext=(-30, 30),
+                     textcoords='offset points',
+                     ha='right',
+                     va='bottom',
+                     bbox=dict(boxstyle='round,pad=1.', fc='yellow', alpha=0.5),
+                     arrowprops = dict(arrowstyle='->', connectionstyle='arc3,rad=0'))
+        #a.set_visible(False)
+        annotations.append(a)
 
-    # This is identical to the scatter exercise, but adds the 'source' parameter
-    p.circle(x, y, radius=radii, source=source,
-                    fill_color=colors, fill_alpha=0.6, line_color=None)
+    def onpick3(event):
+        for a in annotations:
+            a.set_visible(False)
+        for ind in event.ind:
+            annotations[ind].set_visible(True)
+        fig.canvas.draw()
+        return True
 
-    # EXERCISE (optional) add a `text` renderer to display the index of each circle
-    # inside the circle
-    p.text(x, y, text=names, alpha=0.5, text_font_size="5pt",
-                text_baseline="middle", text_align="center")
-
-    # EXERCISE: try other "marker-like" renderers besides `circle`
-
-    # We want to add some fields for the hover tool to interrogate, but first we
-    # have to get ahold of the tool. We can use the 'select' method to do that.
-    hover = p.select(dict(type=HoverTool))
-
-    # EXERCISE: add some new tooltip (name, value) pairs. Variables from the
-    # data source are available with a "@" prefix, e.g., "@x" will display the
-    # x value under the cursor. There are also some special known values that
-    # start with "$" symbol:
-    #   - $index     index of selected point in the data source
-    #   - $x, $y     "data" coordinates under cursor
-    #   - $sx, $sy   canvas coordinates under cursor
-    #   - $color     color data from data source, syntax: $color[options]:field_name
-    # NOTE: tooltips will show up in the order they are in the list
-    hover.tooltips = [
-        # add to this
-        ("(x,y)", "($x, $y)"),
-        ("radius", "@radius"),
-        ("name", "@name"),
-    ]
-    show(p)
+    fig.canvas.mpl_connect('pick_event', onpick3)
+    plt.show()