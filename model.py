add32dfb9b897a21d16445c6ef30491dd8322295
Mehdi Cherti <mehdi@cherti.name>
2015-05-05 23:04:56 +0200

    support pca for fitting; choice for model representation; support inversing from embedding to predictions by PCA then using the predictions from the inversions to learn a new model instead of backpropagating directly the error from the embedding

--- model.py
+++ model.py
@@ -1,12 +1,15 @@
 import mds
 import theano
 import theano.tensor as T
-from lasagne import easy, updates, layers, init, nonlinearities, objectives
+from lasagne import easy, updates, layers, init, nonlinearities
 import numpy as np
 from sklearn.manifold import MDS
+from sklearn.decomposition import PCA
 
 
 from lasagne.easy import LightweightModel
+
+
 def ensemble_machine_model_loss(y_new, z_new,
                                 y_old, z_old,
                                 dist_y_func, dist_z_func):
@@ -25,6 +28,22 @@ def ensemble_machine_model_loss(y_new, z_new,
     return mds.MDS_loss(dist_y, dist_z)
 
 
+def ensemble_machine_model_loss_given_y(y, wanted_y):
+    return (((y - wanted_y) ** 2).sum(axis=1)).mean()
+
+
+def model_representation_by_increasing_mean(probas, context=T):
+    return probas * context.arange(probas.shape[1]).sum(axis=1)
+
+
+def model_representation_by_labels(probas, context=T):
+    return probas.argmax(axis=1)
+
+
+def model_representation_by_probas(probas, context=T):
+    return probas.flatten()
+
+
 class EnsembleMachine(object):
 
     def __init__(self, n_components=2,
@@ -41,15 +60,30 @@ class EnsembleMachine(object):
 
         self.Z = None
 
-    def fit(self, Y):
-        mds = MDS(n_components=2, dissimilarity='precomputed')
+        self.inverser_ = None
 
-        #dis = (Y[:, :, np.newaxis] != Y.T[np.newaxis, :, :]).sum(axis=1) # hamming
-        dis =  np.sqrt(((Y[:, :, np.newaxis] - Y.T[np.newaxis, :, :])**2).sum(axis=1))
-        Z = mds.fit_transform(dis)
+    def fit_with_pca(self, Y):
+        assert (self.dist_y == mds.euclidian_dist and
+                self.dist_z == mds.euclidian_dist)
+        self.inverser_ = PCA(n_components=2)
+        Z = self.inverser_.fit_transform(Y)
+        self.Y = theano.shared(Y, borrow=True)
+        self.Z = theano.shared(Z, borrow=True)
+
+    def fit_with_mds(self, Y):
+        assert self.dist_z == mds.euclidian_dist
+
+        mds_ = MDS(n_components=2, dissimilarity='precomputed')
+        if self.dist_y == mds.hamming_dist:
+            dis = (Y[:, :, np.newaxis] != Y.T[np.newaxis, :, :]).sum(axis=1) # hamming
+        elif self.dist_y == mds.euclidian_dist:
+            dis =  np.sqrt(((Y[:, :, np.newaxis] - Y.T[np.newaxis, :, :])**2).sum(axis=1))
+        Z = mds_.fit_transform(dis)
         self.Y = theano.shared(Y, borrow=True)
         self.Z = theano.shared(Z, borrow=True)
         return self
+
+    def fit_with_gradient_descent(self, Y):
 
         nb_examples = Y.shape[0]
 
@@ -91,10 +125,12 @@ class EnsembleMachine(object):
         self.Y = Y
         return self
 
-    def update(self, X, y, models_new, Z_new, batch_optimizer=None, lambda_=1.):
+    def update_with_gradient_descent(self, X, y, models_new, Z_new,
+                                     batch_optimizer=None, lambda_=1.,
+                                     model_repr=model_representation_by_probas,
+                                     inverser=False):
         assert X.shape[0] == y.shape[0]
         assert len(models_new) == Z_new.shape[0]
-
 
         nb_examples = X.shape[0]
         if batch_optimizer is None:
@@ -115,17 +151,22 @@ class EnsembleMachine(object):
         for model in models_new:
             output, = model.get_output(X_batch)
             probas.append(output)
-            #output = T.argmax(output, axis=1)
-            output = (output * T.arange(model.output_layers[0].num_units)).sum(axis=1)
-            #output = T.cast(output, theano.config.floatX)
+            output = model_repr(output)
             output = output.dimshuffle('x', 0)
             outputs.append(output)  # (1, nb examples)
 
         # (nb_new_models, nb_examples)
         Y_new = T.concatenate(outputs, axis=0)
-        loss_ensemble_machine = ensemble_machine_model_loss(Y_new, Z_new,
-                                                            self.Y, self.Z,
-                                                            self.dist_y, self.dist_z)
+
+        if inverser == False:
+            loss_ensemble_machine = ensemble_machine_model_loss(Y_new, Z_new,
+                                                                self.Y, self.Z,
+                                                                self.dist_y, self.dist_z)
+        else:
+            assert self.inverser_ is not None
+            Y_wanted = self.inverser_.inverse_transform(Z_new)
+            loss_ensemble_machine = ensemble_machine_model_loss_given_y(Y_new, Y_wanted)
+
         loss_accuracy = 0.
         for pr in probas:
             loss_accuracy += -T.mean(T.log(pr)[T.arange(y_batch.shape[0]), y_batch])
@@ -178,11 +219,13 @@ class EnsembleMachine(object):
         return self
 
 
-def build_Y(X, models, n_classes):
-    Y = np.zeros((len(models), X.shape[0]))
-    for i, (name, model) in enumerate(models):
-        Y[i, :] = (model.predict_proba(X) * np.arange(n_classes)).sum(axis=1)
-        #Y[i, :] = model.predict(X)
+def build_Y(X, models, n_classes, model_repr=model_representation_by_probas):
+    name, model = models[0]
+    r = model_repr(model.predict_proba(X), context=np)
+    Y = np.zeros((len(models), r.shape[0]))
+    Y[0] = r
+    for i, (name, model) in enumerate(models[1:]):
+        Y[i + 1, :] = model_repr(model.predict_proba(X), context=np)
     return Y
 
 if __name__ == "__main__":
@@ -204,7 +247,10 @@ if __name__ == "__main__":
     light.file_snapshot()
     light.tag("ensemble_model_example")
 
-    np.random.seed(0)
+    seed = 0
+    np.random.seed(seed)
+    light.set_seed(seed)
+
     y_dim = 4
     n_classes = y_dim
     X, y = make_classification(100, n_classes=y_dim, n_informative=10)
@@ -236,9 +282,8 @@ if __name__ == "__main__":
     es = EnsembleMachine(n_components=2,
                          dist_y=mds.euclidian_dist,
                          batch_optimizer=batch_optimizer)
-    es.fit(Y)
+    es.fit_with_pca(Y)
     plot_model_embeddings(models, es.Z.get_value(), save_file="before.png")
-
 
     x_in = layers.InputLayer(shape=(None, X.shape[1]))
     h = layers.DenseLayer(x_in, num_units=200,
@@ -254,7 +299,7 @@ if __name__ == "__main__":
                              [z_out])
     models_new = [model]
     Z_new = [
-        [6, -1]
+        [-3, 1.96]
     ]
     Z_new = np.array(Z_new, dtype=theano.config.floatX)
 
@@ -265,9 +310,9 @@ if __name__ == "__main__":
                     print(es.get_dist_y(X))
                     print(es.get_dist_z())
 
-                loss_ensemble_machine = es.get_loss_ensemble_machine(X)
-                loss_accuracy = es.get_loss_accuracy(X, y)
-                loss = es.get_loss(X, y)
+                loss_ensemble_machine = float(es.get_loss_ensemble_machine(X))
+                loss_accuracy = float(es.get_loss_accuracy(X, y))
+                loss = float(es.get_loss(X, y))
                 light.append("loss_ensemble_machine", loss_ensemble_machine)
                 light.append("loss_accuracy", loss_accuracy)
                 light.append("loss", loss)
@@ -278,15 +323,16 @@ if __name__ == "__main__":
     batch_optimizer = MyBatchOptimizer(verbose=1,
                                        max_nb_epochs=100,
                                        optimization_procedure=(updates.nesterov_momentum, {"learning_rate": 0.001, "momentum": 0.8}))
-    es.update(X, y, models_new, Z_new,
-              batch_optimizer=batch_optimizer,
-              lambda_=0.1)
+    es.update_with_gradient_descent(X, y, models_new, Z_new,
+                                    batch_optimizer=batch_optimizer,
+                                    lambda_=0.1,
+                                    inverser=True)
     models_updated = models + [("new_%d" % (i,), es) for i, m in enumerate(models_new)]
     Y = build_Y(X, models_updated, n_classes)
     Y = Y.astype(theano.config.floatX)
-    es = EnsembleMachine(n_components=2,
-                         dist_y=mds.euclidian_dist)
-    es.fit(Y)
+    es = EnsembleMachine(n_components=2)
+
+    es.fit_with_pca(Y)
     plot_model_embeddings(models_updated, es.Z.get_value(), save_file="after.png")
 
     light.endings()
